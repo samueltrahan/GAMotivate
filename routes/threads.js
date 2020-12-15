4e2f6d8b82eedc65515b64663d1e42ff36f41d97
Samuel Gemberling <sgembeling@gmail.com>
2020-12-14 20:56:42 -0600

    more work on backend

--- routes/threads.js
+++ routes/threads.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const threadCtrl = require('../controllers/threads')
 
 router.get('/:id', threadCtrl.getThreadDetails)
+
+// router.use(require('../config/auth'))
+
 router.post('', threadCtrl.create)
 router.post('/:id', threadCtrl.addReply)
+router.put('/:id', threadCtrl.update)
+router.delete('/:id', threadCtrl.deleteOne)
 
 module.exports = router
