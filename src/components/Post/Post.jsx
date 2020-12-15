bc265d4728cf126a4568ce401701f8495e4987e7
Samuel Gemberling <sgembeling@gmail.com>
2020-12-15 13:39:03 -0600

    fix creating thread

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,21 +1,25 @@
 import React from "react"
 import "./Post.css"
 
-const Post = (props) => {
+const Post = ({ post }) => {
     return (
         <>
-            <section className="post">
-                <div>
-                    <img src="#" alt="avatar" />
-                    <p>username</p>
-                    <p>{props.cohort ? props.cohort : "no cohort"}</p>
-                </div>
-                <div className="message">
-                    <p>{props.message ? props.message : "no message"}</p>
-                    <button>Ask</button>
-                    <button>Motivate</button>
-                </div>
-            </section>
+            {post ? (
+                <section className="post">
+                    <div>
+                        <img src="" alt="avatar" />
+                        <p>{post.postedBy.name}</p>
+                        <p>{post.cohort ? post.cohort : "no cohort"}</p>
+                    </div>
+                    <div className="message">
+                        <p>{post.message}</p>
+                        <button>Ask</button>
+                        <button>Motivate</button>
+                    </div>
+                </section>
+            ) : (
+                ""
+            )}
         </>
     )
 }
