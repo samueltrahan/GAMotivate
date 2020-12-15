a5adf3dc6092b76460629dbd94a1fc6b41c4435e
Molly Haughey <mollyhaughey@Mollys-MBP.cable.rcn.com>
2020-12-15 17:08:34 -0500

    formatting userpage

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import userService from '../../services/userService';
+import "./UserPage.css"
 
-class UserPage extends React.Component {
-  state = {
-    show: false
-  };
-  
-  render() { 
-    return (
+export default function UserPage({user}) {
+  const [users, setUsers] = useState({})
+  useEffect(() =>{
+    const userId = userService.getUserFromId(
+      user._id
+    )
+    setUsers(userId)
+  }, [])
+
+  return (
       <div className="User">
-        <h1>Welcome!</h1>
-        <a href="#portfolio" target="_blank">Portfolio</a><br/>
-        <a href="#linkedin" target="_blank"><img src="https://i.imgur.com/rPkZjhu.png" width="50"/></a>        
+        <div className="inner-div">
+          <div className="background-color-div">
+            <img src="img" alt="img"/><br/>
+            <button>Edit Profile</button><br/>
+            <p><b>{users.name}</b></p><br/>
+            <p>{users.job}</p><br/>
+            <p><b>{users.cohort}</b></p><br/>
+            <p><b>{users.status}</b></p><br/>
+            <p>{users.location}</p><br/>
+          </div>
+          <div className="imgs">
+            <img src="https://i.imgur.com/fm34YyM.png" width="18" alt="world"/>              <a href="#portfolio" target="_blank" >Portfolio link</a><br/>
+            <img src="https://i.imgur.com/c9Rrzpx.png" alt="linkedin" width="18"/>         <a href="#linkedin" target="_blank">LinkedIn link</a><br/>
+            <img src="https://i.imgur.com/LkBsi3O.png" alt="notifcations" width="18"/> Notifications
+          </div>  
+        </div>     
       </div>
     );
-  }
 }
-
-export default UserPage;
